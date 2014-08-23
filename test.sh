88e3c1ce341b16d8ad2e7ffb3cbb8920c8edcaf7
Timo Heister <timo.heister@gmail.com>
2014-08-23 15:48:04 -0400

    update test.sh to create a summary logfile

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-submit="OFF"
+#submit="OFF"
 submit="ON"
 sha=$1
 name=$2
@@ -10,20 +10,26 @@ basepath=$1
 build=$2
 sha=$3
 name=$4
-logfile=$1/logs/$3.$2
-rm -f $logfile
+logfile=$basepath/logs/$sha/$build
+summary=$basepath/logs/$sha/summary
 
-echo "BUILD $build:"
+echo "BUILD $build:" >>$summary
 cd $basepath
 ./setup-$build.sh
 cd build-$build
 nice ctest -S ../aspect/tests/run_testsuite.cmake -DDESCRIPTION="$build$name" -Dsubmit=$submit -V -j 10 >$logfile 2>&1
-grep "Compiler errors" $logfile
-grep "Compiler warnings" $logfile
-grep "tests passed" $logfile
+grep "Compiler errors" $logfile >>$summary
+grep "Compiler warnings" $logfile >>$summary
+if [ "$build" != "clang" ]
+then
+  grep "tests passed" $logfile >>$summary
+fi
 }
 
 basepath=`pwd`
+
+mkdir -p $basepath/logs/$sha
+rm -f $basepath/logs/$sha/*
 
 build="clang"
 output $basepath $build $sha $name
@@ -34,4 +40,5 @@ output $basepath $build $sha $name
 build="gccpetsc"
 output $basepath $build $sha $name
 
+cat $basepath/logs/$sha/summary
 
