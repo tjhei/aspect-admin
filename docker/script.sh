a4d59e99a0e6e86bbbabc09d49645e5753df64f8
Timo Heister <timo.heister@gmail.com>
2014-12-06 12:17:34 -0500

    update script.sh

--- docker/script.sh
+++ docker/script.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo "hello world"
-ls /aspect-tester
+echo "ASPECT testing - hello from script.sh"
+if [ -f /aspect-tester/runner.py ];
+then
+       cd /aspect-tester; python runner.py
+else
+       echo "please mount /aspect-tester"
+fi
