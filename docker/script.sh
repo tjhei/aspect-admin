7730057aa3c486e729d2d62afce0a41074e8ea97
Timo Heister <timo.heister@gmail.com>
2014-12-06 12:20:29 -0500

    pass arguments

--- docker/script.sh
+++ docker/script.sh
@@ -3,7 +3,7 @@
 echo "ASPECT testing - hello from script.sh"
 if [ -f /aspect-tester/runner.py ];
 then
-       cd /aspect-tester; python runner.py
+       cd /aspect-tester; python runner.py "$@"
 else
        echo "please mount /aspect-tester"
 fi
